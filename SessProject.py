2fbaf7db406ba4b5d1cda0ced1837998e08e1358
Hadi Jafari <sig.haadijafari@gmail.com>
2023-02-04 08:32:00 +0330

    v. 1.0 Completed
    [+] main.py created in order to be run
    [+] functions are created
    [+] clicking on display button bug has been fixed
    [+] finding units by odd and even row changed to finding them with unit code
    [+] every department has its own csv file now
    
    soon: unnecessary units will not be saved
    soon: passed units will not be saved

--- SessProject.py
+++ SessProject.py
@@ -1,66 +1,61 @@
 from time import sleep
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 import csv
 
-sess = 'https://sess.sku.ac.ir/'
-user_pass = []
-with open('user_pass.txt', 'r') as file:
-    user_pass.append(file.readline())
-    user_pass.append(file.readline())
 
-driver = webdriver.Chrome()
-driver.set_window_position(-10000, 0)
-driver.get(sess)
-driver.find_element(By.NAME, "edId").send_keys(user_pass[0])
-driver.find_element(By.ID, "edPass").send_keys(user_pass[1])
-driver.find_element(By.ID, "edEnter")._execute()
-# [0] = Unit select | [1] = Other Actions | [2] = Exams | [3] = Sign up allowed check
-driver.find_elements(By.TAG_NAME, "h6")[1].click()
+def logIn(driver):
+    sess = 'https://sess.sku.ac.ir/'
+    user_pass = []
+    with open('user_pass.txt', 'r') as file:
+        user_pass.append(file.readline())
+        user_pass.append(file.readline())
+    driver.get(sess)
+    driver.find_element(By.NAME, "edId").send_keys(user_pass[0])
+    driver.find_element(By.ID, "edPass").send_keys(user_pass[1])
+    sleep(0.5)
+    display = driver.find_element(By.ID, "edEnter")
+    display.click()
+    # [0] = Unit select | [1] = Other Actions | [2] = Exams | [3] = Sign up allowed check
+    driver.find_elements(By.TAG_NAME, "h6")[1].click()
 
-# آموزشی
-sleep(1)
-driver.find_element(By.XPATH, "//label[@for=\'group-3\']").click()
-sleep(1)
-# لیست دروس نیمسال
-driver.find_elements(By.TAG_NAME, 'li')[28].click()
+def SemesterUnitListsPage(driver):
+    # آموزشی
+    sleep(1)
+    driver.find_element(By.XPATH, "//label[@for=\'group-3\']").click()
+    sleep(1)
+    # لیست دروس نیمسال
+    driver.find_elements(By.TAG_NAME, 'li')[28].click()
 
-Departments = ['بخش علوم كامپيوتر', 'بخش رياضي كاربردي', '']
-for Dep in Departments:
-    Select(driver.find_element(By.ID, 'edSemester')
-           ).select_by_visible_text('دوم - 1401')
-    Select(driver.find_element(By.ID, 'edDepartment')
-           ).select_by_visible_text(Dep)
-    driver.find_element(By.ID, 'edDisplay').click()
-    doros = driver.find_elements(
-        By.CLASS_NAME, 'listOdd') + driver.find_elements(By.CLASS_NAME, 'listEven')
+def ScrapeUnits(driver, fields, Departments):
+    for Dep in Departments:
+        dep_units = []
+        Select(driver.find_element(By.ID, 'edSemester')
+            ).select_by_visible_text('دوم - 1401')
+        Select(driver.find_element(By.ID, 'edDepartment')
+            ).select_by_visible_text(Dep)
+        driver.find_element(By.ID, 'edDisplay').click()
+        sleep(0.5)
+        doros = driver.find_elements(By.XPATH, "//td[@align=\'center\']")
 
-    all_units = []
-    for i in range(len(doros)):
-        doros[i].click()
-        html = html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        all_units.append([x.text for x in soup.find_all('div')[7:13]] +
-                         [x.text for x in soup.find_all('div')[18:23]])
-        driver.back()
-        doros = driver.find_elements(
-            By.CLASS_NAME, 'listOdd') + driver.find_elements(By.CLASS_NAME, 'listEven')
+        for i in range(1, len(doros)):
+            doros[i].click()
+            html = html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            dep_units.append([x.text for x in soup.find_all('div')[7:13]] +
+                            [x.text for x in soup.find_all('div')[18:23]])
+            driver.back()
+            doros = driver.find_elements(By.XPATH, "//td[@align=\'center\']")
 
-    fields = ['Unit name',
-              'Unit count',
-              'Unit code',
-              'Group',
-              'Class type',
-              'Professor',
-              'Time and Place',
-              'Midterm date',
-              'Midterm time',
-              'Final date',
-              'Final time']
-    for i in all_units:
-        with open('Units.csv', 'a', encoding="utf-8-sig") as f:
+        CSVWriter(fields,dep_units,Dep)
+            
+        
+        
+            
+def CSVWriter(fields, data, Dep):
+    for i in data:
+        with open('%s.csv' % Dep, 'w', encoding="utf-8-sig") as f:
             write = csv.writer(f)
             write.writerow(fields)
-            write.writerows(all_units)
+            write.writerows(data)
---
+++ main.py
@@ -0,0 +1,24 @@
+from SessProject import *
+from selenium import webdriver
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    driver.set_window_position(-10000, 0)
+
+    Departments = ['بخش علوم كامپيوتر', 'بخش رياضي كاربردي', 'بخش معارف اسلامي']
+    fields = ['Unit name',
+                'Unit count',
+                'Unit code',
+                'Group',
+                'Class type',
+                'Professor',
+                'Time and Place',
+                'Midterm date',
+                'Midterm time',
+                'Final date',
+                'Final time']
+
+            
+    logIn(driver)
+    SemesterUnitListsPage(driver)
+    ScrapeUnits(driver, fields, Departments)
